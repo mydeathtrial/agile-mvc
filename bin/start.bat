70797c545946d3e3cc4f1f3806c0c718fba410d4
tongmeng@idss-cn.com <tongmeng@idss-cn.com>
2020-06-17 05:51:29 +0800

    feat(新增启动脚本):

--- bin/start.bat
+++ bin/start.bat
@@ -8,23 +8,30 @@ cd ..\bin
 
 set "mode=production"
 set "main-class=com.idss.Application"
+set "hidden=no"
 
 set /p main-class=Program main class, default ( com.idss.Application ) :
 set /p mode=Execution mode of application, default ( production ):
+set /p hidden=hidden, yes or no, default ( no ):
 
+if "%hidden%" == "yes" (
+    set java="javaw"
+) else (
+    set java="java"
+)
 
 if "%mode%" == ""debug"" goto debug
 if "%mode%" == ""jmx"" goto jmx
 
-java -Xms64m -Xmx1024m -XX:MaxPermSize=64M -classpath ..\conf;%LIB_JARS% %main-class%
+%java% -Xms64m -Xmx1024m -XX:MaxPermSize=64M -classpath ..\conf;%LIB_JARS% %main-class%
 goto end
 
 :debug
-java -Xms64m -Xmx1024m -XX:MaxPermSize=64M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -classpath ..\conf;%LIB_JARS% %main-class%
+%java% -Xms64m -Xmx1024m -XX:MaxPermSize=64M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -classpath ..\conf;%LIB_JARS% %main-class%
 goto end
 
 :jmx
-java -Xms64m -Xmx1024m -XX:MaxPermSize=64M -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -classpath ..\conf;%LIB_JARS% %main-class%
+%java% -Xms64m -Xmx1024m -XX:MaxPermSize=64M -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -classpath ..\conf;%LIB_JARS% %main-class%
 
 :end
 pause
