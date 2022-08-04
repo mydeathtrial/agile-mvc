a84633f325c199509d272daa7bbc8ca546dd319f
mydeathtrial <mydeathtrial@163.com>
2022-08-04 18:23:04 +0800

    build:

---
+++ src/main/java/cloud/agileframework/mvc/provider/DataExtendManager.java
@@ -0,0 +1,17 @@
+package cloud.agileframework.mvc.provider;
+
+/**
+ * @author 佟盟
+ * 日期 2020/7/15 19:17
+ * 描述 TODO
+ * @version 1.0
+ * @since 1.0
+ */
+public interface DataExtendManager {
+    /**
+     * 字典转换工具
+     *
+     * @param o 转换前的对象
+     */
+    void cover(Object o);
+}
---
+++ src/main/java/cloud/agileframework/mvc/provider/DictionaryProvider.java
@@ -0,0 +1,17 @@
+package cloud.agileframework.mvc.provider;
+
+import cloud.agileframework.dictionary.util.DictionaryUtil;
+
+/**
+ * @author 佟盟
+ * 日期 2020/8/4 9:42
+ * 描述 为JPA组件提供字典翻译扩展
+ * @version 1.0
+ * @since 1.0
+ */
+public class DictionaryProvider implements DataExtendManager {
+    @Override
+    public void cover(Object o) {
+        DictionaryUtil.cover(o);
+    }
+}
